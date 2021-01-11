2579d7775727ffc7c28977632bdfc1d38a0be83b
E <throw2122@protonmail.com>
2021-01-10 21:32:53 -0800

    Update 8x8x8_cube.cpp

--- LED_CUBE_CURRENT/src/8x8x8_cube.cpp
+++ LED_CUBE_CURRENT/src/8x8x8_cube.cpp
@@ -116,7 +116,7 @@ setNode and clearNode
 
 -> (PARAM): x, y, z: The address (physical location) of bit to be set or cleared
 
--> (OUTPUT): set or clear node at value x, y, z
+-> (OUTPUT): set or clear specific node at value x, y, z
 *//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setNode(int x, int y, int z){ //set a specified node at x, y, z
   if ((inRange(x)) && (inRange(y)) && (inRange(z))){ //make sure that the node to be set is in range of the cube and buffer
@@ -127,77 +127,163 @@ void clearNode(int x, int y, int z){ //clear a specified node at x,y,z
   cube[y][z] &= ~(1<<x);}} //run bitwise operation to set the specified bit
 
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+set_col
+
+-> (OUTPUT): Sets whole column based on specified x,y location
+
+-> (PARAMS): Takes int x and int y values
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void set_col(int x, int y){ //set whole column
   int i;
-  for(i=0; i < 8; i++){
-    setNode(x, y, i);
+  for(i=0; i < 8; i++){ 
+    setNode(x, y, i); //set a node at specified x,y value through all 8 layers
     }}
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+clear_col
+
+-> (OUTPUT): Clear whole column based on specified x,y location
+
+-> (PARAMS): Takes int x and int y values
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void clear_col(int x, int y){ //clear whole column
   int i;
   for(i=0; i < 8; i++){
-    clearNode(x, y, i);
+    clearNode(x, y, i); //clear a node at specified x,y value through all 8 layers
     }}
 
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+set_row
+
+-> (OUTPUT): Set whole row down the x axis of nodes
+
+-> (PARAMS): Takes int y and int z values
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void set_row(int y, int z){ //set row down x axis
   int i;
   for(i=0; i<8; i++){
-    setNode(i, y, z);
+    setNode(i, y, z); //set node at specified y,z value through all 8 nodes down the x axis
     }}
 
-  
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+clear_row
+
+-> (OUTPUT): Clear whole row down the x axis of nodes
+
+-> (PARAMS): Takes int y and int z values
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void clear_row(int y, int z){  //clear row down x axis
   int i;
   for(i=0; i<8; i++){
-    clearNode(i, y, z);
+    clearNode(i, y, z);//clear node at specified y,z value through all 8 nodes down the x axis
     }}
+
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+set_ray
+
+-> (OUTPUT): Set whole row down the y axis of nodes
+
+-> (PARAMS): Takes int x and int z values
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void set_ray(int x, int z){  //set row down y axis
   int i;
   for(i=0; i<8; i++){
-    setNode(x, i, z);
+    setNode(x, i, z); //set node at specified x,z value through all 8 nodes down the y axis
     }}
 
-  
+
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+clear_ray
+
+-> (OUTPUT): Clear whole row down the y axis of nodes
+
+-> (PARAMS): Takes int x and int z values
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void clear_ray(int x, int z){ //clear row down y axis
   int i;
   for(i=0; i<8; i++){
-    clearNode(x, i, z);
+    clearNode(x, i, z);//clear node at specified x,z value through all 8 nodes down the y axis
     }}
 
     
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+set_plane
+
+-> (OUTPUT): Set whole 2D plane with specified orientation
+
+-> (PARAMS): Takes orientation and the starting position of the 2D plane
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void set_plane(int orientation, int start_point){
-  if (orientation == x_y){
+  if (orientation == x_y){ //if the orientation is set vertically
     int i;
     for(i = 0; i < 8; i++){
-      set_col(i, start_point);
+      set_col(i, start_point); //set columns down the x axis at specified y value
       }
     }
-     if (orientation == y_z){
+     if (orientation == y_z){ //if the orientation is set horizontally
     int n;
     for(n = 0; n < 8; n++){
-      set_ray(n, start_point);
+      set_ray(n, start_point); //set rays down the x axis at specified z value
       }
     }
   }
 
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+clear_plane
+
+-> (OUTPUT): Clear whole 2D plane with specified orientation
+
+-> (PARAMS): Takes orientation and the starting position of the 2D plane
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void clear_plane(int orientation, int start_point){
-  if (orientation == x_y){
+  if (orientation == x_y){ //if the orientation is set vertically
     int i;
     for(i = 0; i < 8; i++){
-      clear_col(i, start_point);    
+      clear_col(i, start_point);     //clear columns down the x axis at specified y value
       }
     } 
 
-    if (orientation == y_z){
+    if (orientation == y_z){  //if the orientation is set horizontally
     int n;
     for(n = 0; n < 8; n++){
-      clear_ray(n, start_point);    
+      clear_ray(n, start_point);    //clear rays down the x axis at specified z value
       }
     } 
   }
 
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+clear_cube
+
+-> (OUTPUT): Clears whole cube in all 3 axis (Turns off all LED's)
+
+-> (PARAMS): NONE
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void clear_cube(){
   int i;
   for (i=0; i<8; i++){
@@ -205,27 +291,58 @@ void clear_cube(){
   }
 }  
 
+
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+set_random
+
+-> (OUTPUT): sets a specified number of random nodes on in a specified range, if node is already on it remains on
+
+-> (PARAMS): int numRand: number of random nodes to be set on 
+            int range: value in between 0 and 8 inclusive to be the absolute range the random values are allowed to be in
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void set_random(int numRand, int range){
 int i;
-for(i=0;i<numRand;i++){
-  int randX = random(0, range);
-  int randY = random(0, range);
-  int randZ = random(0, range);
-  setNode(randX, randY, randZ);
+for(i=0;i<numRand;i++){ 
+  int randX = random(0, range); //random x value
+  int randY = random(0, range); //random y value
+  int randZ = random(0, range); //random z value
+  setNode(randX, randY, randZ); //set a node on with the random x,y,z values
 }
 }
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+clear_random
+
+-> (OUTPUT): clear a specified number of random nodes on in a specified range, if node is already off it remains off
+
+-> (PARAMS): int numRand: number of random nodes to be set off
+            int range: value in between 0 and 8 inclusive to be the absolute range the random values are allowed to be in
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void clear_random(int numRand, int range){
 int i;
 for(i=0;i<numRand;i++){
-  int randX = random(0, range);
-  int randY = random(0, range);
-  int randZ = random(0, range);
-  clearNode(randX, randY, randZ);
+  int randX = random(0, range);//random x value
+  int randY = random(0, range);//random y value
+  int randZ = random(0, range);//random z value
+  clearNode(randX, randY, randZ);//set a node off with the random x,y,z values
 }
 }
 
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+full_test_demo
+
+-> (OUTPUT): A preprogramed sequence which sets each plane on and off one by one in both the veritcal (x_y) and horizontal (x_z) directions
+
+-> (PARAMS): NONE
+
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void full_test_demo(){
 set_plane(x_y, 0);
 frame(delay_amount, 100,layer);
@@ -312,279 +429,186 @@ frame(delay_amount, 100,layer);
 }
 
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+H
+-> (OUTPUT): Draws a letter H in the x_z direction at specified y value
 
+-> (PARAMS): int i: y value for the letter to be drawn on
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void H (int i){
-  set_col(1,i);
+set_col(1,i);
 set_col(0,i);
-
 set_col(6,i);
 set_col(7,i);
-
 set_row(i,4);
 set_row(i,3);
-frame(delay_amount, 150,layer);
-
+frame(delay_amount, 50,layer);
 clear_cube();
 }
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+I
+-> (OUTPUT): Draws a letter I in the x_z direction at specified y value
+
+-> (PARAMS): int i: y value for the letter to be drawn on
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void I (int i){
-  set_col(4,i);
+set_col(4,i);
 set_col(3,i);
-
-frame(delay_amount, 150,layer);
+frame(delay_amount, 50,layer);
 clear_cube();
 }
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+R
+-> (OUTPUT): Draws a letter R in the x_z direction at specified y value
+
+-> (PARAMS): int i: y value for the letter to be drawn on
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void R (int i){
   
-set_col(7,i);
-set_col(6,i);
-
-set_col(3,i);
-set_col(2,i);
+set_col(0,i);
+set_col(1,i);
+set_col(4,i);
+set_col(5,i);
 set_row(i,7);
 set_row(i,6);
-
 set_row(i,3);
 set_row(i,2);
-
-clearNode(0,i,7);
-clearNode(1,i,7);
-clearNode(0,i,6);
-clearNode(1,i,6);
-
-clearNode(2,i,7);
-clearNode(3,i,7);
-clearNode(2,i,6);
-clearNode(3,i,6);
-
-clearNode(0,i,3);
-clearNode(1,i,3);
-clearNode(0,i,2);
-clearNode(1,i,2);
-
-clearNode(2,i,3);
-clearNode(3,i,2);
-clearNode(3,i,3);
-clearNode(2,i,2);
-
-
-frame(delay_amount, 100,layer);
+clearNode(7,i,7);
+clearNode(6,i,7);
+clearNode(7,i,6);
+clearNode(6,i,6);
+clearNode(5,i,7);
+clearNode(4,i,7);
+clearNode(5,i,6);
+clearNode(4,i,6);
+clearNode(7,i,3);
+clearNode(6,i,3);
+clearNode(7,i,2);
+clearNode(6,i,2);
+clearNode(5,i,3);
+clearNode(4,i,2);
+clearNode(5,i,3);
+clearNode(4,i,2);
+frame(delay_amount, 50,layer);
 clear_cube();
 }
 
-void E (int i){
-  set_col(7,i);
-set_col(6,i);
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+E
+-> (OUTPUT): Draws a letter E in the x_z direction at specified y value
 
+-> (PARAMS): int i: y value for the letter to be drawn on
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void E (int i){
+set_col(0,i);
+set_col(1,i);
 set_row(i,7);
 set_row(i,6);
-
-
 set_row(i,0);
 set_row(i,1);
 set_row(i,4);
 set_row(i,3);
-
-clearNode(0,i,7);
-clearNode(1,i,7);
-clearNode(0,i,6);
-clearNode(1,i,6);
-
-clearNode(0,i,1);
-clearNode(1,i,1);
-clearNode(0,i,0);
-clearNode(1,i,0);
-
-
-clearNode(0,i,4);
-clearNode(1,i,4);
-clearNode(0,i,3);
-clearNode(1,i,3);
-
-frame(delay_amount, 100,layer);
+clearNode(7,i,7);
+clearNode(6,i,7);
+clearNode(7,i,6);
+clearNode(6,i,6);
+clearNode(7,i,1);
+clearNode(6,i,1);
+clearNode(7,i,0);
+clearNode(6,i,0);
+clearNode(7,i,4);
+clearNode(6,i,4);
+clearNode(7,i,3);
+clearNode(6,i,3);
+frame(delay_amount, 50,layer);
 clear_cube();
 
 }
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+M
+-> (OUTPUT): Draws a letter M in the x_z direction at specified y value
+
+-> (PARAMS): int i: y value for the letter to be drawn on
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void M (int i){
 
 set_col(7,i);
 set_col(6,i);
 set_col(0,i);
 set_col(1,i);
-
-
-setNode(3,i,5);
+setNode(2,i,5);
+setNode(5,i,5);
 setNode(2,i,4);
 setNode(3,i,4);
+setNode(4,i,4);
+setNode(5,i,4);
+setNode(3,i,3);
+setNode(4,i,3);
+frame(delay_amount, 50,layer);
+clear_cube();
+}
+
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+happy_face
+-> (OUTPUT): Draws a happy face in the x_z direction at specified y value
+
+-> (PARAMS): int i: y value for the letter to be drawn on
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void happy_face(int i){
+
+
+setNode(3,i,4);
+setNode(4,i,4);
+setNode(3,i,3);
+setNode(4,i,3);
+
+setNode(1,i,5);
+setNode(2,i,5);
+setNode(1,i,6);
+setNode(2,i,6);
 
 
 setNode(5,i,5);
-setNode(4,i,4);
-setNode(5,i,4);
+setNode(6,i,5);
+setNode(5,i,6);
+setNode(6,i,6);
 
-setNode(3,i,3);
-setNode(4,i,3);
-setNode(3,i,2);
-setNode(4,i,2);
+setNode(4,i,0);
+setNode(5,i,0);
+setNode(4,i,1);
+setNode(5,i,1);
+setNode(3,i,0);
+setNode(2,i,0);
+setNode(3,i,1);
+setNode(2,i,1);
 
-frame(delay_amount, 100,layer);
+setNode(0,i,1);
+setNode(1,i,1);
+setNode(0,i,2);
+setNode(1,i,2);
+
+setNode(7,i,1);
+setNode(6,i,1);
+setNode(7,i,2);
+setNode(6,i,2);
+
+frame(delay_amount, 50,layer);
 clear_cube();
+
 
 }
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+hire_me
+-> (OUTPUT): Draws a sequence that reads HIRE ME :) 
 
+-> (PARAMS): NONE
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void hire_me(){
-
-/*
-////////////////H
-set_col(1,7);
-set_col(0,7);
-
-set_col(6,7);
-set_col(7,7);
-
-set_row(7,4);
-set_row(7,3);
-frame(delay_amount, 100,layer);
-
-clear_cube();
-
-
-///////////////////I
-set_col(4,7);
-set_col(3,7);
-
-frame(delay_amount, 100,layer);
-clear_cube();
-
-////////////////R
-
-set_col(7,7);
-set_col(6,7);
-
-set_col(3,7);
-set_col(2,7);
-set_row(7,7);
-set_row(7,6);
-
-set_row(7,3);
-set_row(7,2);
-
-clearNode(0,7,7);
-clearNode(1,7,7);
-clearNode(0,7,6);
-clearNode(1,7,6);
-
-clearNode(2,7,7);
-clearNode(3,7,7);
-clearNode(2,7,6);
-clearNode(3,7,6);
-
-clearNode(0,7,3);
-clearNode(1,7,3);
-clearNode(0,7,2);
-clearNode(1,7,2);
-
-clearNode(2,7,3);
-clearNode(3,7,2);
-clearNode(3,7,3);
-clearNode(2,7,2);
-
-
-frame(delay_amount, 100,layer);
-clear_cube();
-
-/////E
-set_col(7,7);
-set_col(6,7);
-
-set_row(7,7);
-set_row(7,6);
-
-
-set_row(7,0);
-set_row(7,1);
-set_row(7,4);
-set_row(7,3);
-
-clearNode(0,7,7);
-clearNode(1,7,7);
-clearNode(0,7,6);
-clearNode(1,7,6);
-
-clearNode(0,7,1);
-clearNode(1,7,1);
-clearNode(0,7,0);
-clearNode(1,7,0);
-
-
-clearNode(0,7,4);
-clearNode(1,7,4);
-clearNode(0,7,3);
-clearNode(1,7,3);
-
-frame(delay_amount, 100,layer);
-clear_cube();
-
-///M
-
-set_col(7,7);
-set_col(6,7);
-set_col(0,7);
-set_col(1,7);
-
-
-setNode(3,7,5);
-setNode(2,7,4);
-setNode(3,7,4);
-
-
-setNode(5,7,5);
-setNode(4,7,4);
-setNode(5,7,4);
-
-setNode(3,7,3);
-setNode(4,7,3);
-setNode(3,7,2);
-setNode(4,7,2);
-
-frame(delay_amount, 100,layer);
-clear_cube();
-
-//E
-
-set_col(7,7);
-set_col(6,7);
-
-set_row(7,7);
-set_row(7,6);
-
-
-set_row(7,0);
-set_row(7,1);
-set_row(7,4);
-set_row(7,3);
-
-clearNode(0,7,7);
-clearNode(1,7,7);
-clearNode(0,7,6);
-clearNode(1,7,6);
-
-clearNode(0,7,1);
-clearNode(1,7,1);
-clearNode(0,7,0);
-clearNode(1,7,0);
-
-
-clearNode(0,7,4);
-clearNode(1,7,4);
-clearNode(0,7,3);
-clearNode(1,7,3);
-
-frame(delay_amount, 100,layer);
-clear_cube();*/
 
 int i;
 for(i=8; i>=0; i--){
@@ -599,9 +623,16 @@ for(i=8; i>=0; i--){
   M(i);}
 for(i=8; i>=0; i--){
   E(i);}
+for(i=8; i>=0; i--){
+  happy_face(i);}
 }
 
+/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+rand_demo
+-> (OUTPUT): Draws a random sequence of LED lights on and off 
 
+-> (PARAMS): NONE
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void rand_demo(){
 set_random(3,8);
 frame(delay_amount, 30,layer);
